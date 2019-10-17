65c5575b26d885591c602c9ed6d1269f4a59ccca
carrotcorn <47840684+carrotcorn@users.noreply.github.com>
2019-10-17 15:46:19 -0700

    Add files via upload

---
+++ calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            Console.WriteLine("**************CALUCLATOR****************");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("******************MENU******************");
+
+            Console.WriteLine("GIMME A NUMBER BOSS, THE PRESS ENTER");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("GIMME ANOTHER NUMBER BOSS, THEN PRESS ENTA");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("CHOOSE HOW YOU WANNA CALCULATE");
+
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiply");
+            Console.WriteLine("\t/ - Divide");
+            Console.WriteLine("\t% - Divide");
+            Console.Write("Your option? ");
+
+            
+            String userInput = Console.ReadLine();
+
+            if(userInput == "+")
+            {
+                Console.WriteLine($"your result is: " + (num1 + num2));
+            }
+            else if (userInput == "-")
+            {
+                Console.WriteLine($"your result is: " + (num1 - num2));
+            }
+            else if (userInput == "*")
+            {
+                Console.WriteLine($"your result is: " + (num1 * num2));
+            }
+            else if (userInput == "/")
+            {
+                Console.WriteLine($"your result is: " + (num1 / num2));
+            }
+            else if (userInput == "%")
+            {
+                Console.WriteLine($"your result is: " + (num1 % num2));
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
